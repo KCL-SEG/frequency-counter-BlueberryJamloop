dde6ab37670ff839e8fb3120464a9cbef7bf88e5
Maja G <maja293@outlook.com>
2022-10-01 17:23:55 +0100

    Frequencies attempt

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,27 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+items = []
+
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+    frequenciesDict = {}
+    count=0
+    for thing in items:
+        for counting in items:
+            if thing == counting:
+                count += 1
+            else:
+                next
+        frequenciesDict[thing] = count
+        count = 0
+    return frequenciesDict
+
+print("Enter a list of items separated by commas: ")
+for item in input().split(","):
+    items.append(item)
+
+print(frequencies(items))
+
+for x in range (0,len(items)):
+    print(items)
+
